6758057b5c0e1ab0f2d4c7e59660a407ec4b4fbb
Bryan Galván <bryangalvanleon8@gmail.com>
2026-06-16 10:37:59 -0600

    IMPLEMENTACION DE PUT, PATCH, DELETE

--- Diez/app.js
+++ Diez/app.js
@@ -2,13 +2,12 @@ const {infoCursos}=require("./cursos");
 const express=require('express');
 //console.log(JSON.stringify(infoCursos));
 const app=express();
-app.use(express.json());
 const routerMatematicas=require("./routers/routerMatematicas");
 const routerProgramacion=require("./routers/routerProgramacion");
 
 app.use("/api/cursos/programacion",routerProgramacion);
 app.use("/api/cursos/matematicas",routerMatematicas);
-
+routerProgramacion.use(express.json());
 
 
 app.get("/",(req,res)=>{
@@ -20,7 +19,7 @@ app.get("/api/cursos",(req,res)=>{
 });
 
 
-app.post("/api/cursos/programacion",(req,res)=>{
+routerProgramacion.post("/",(req,res)=>{
 
     /*let cuerpo="";
     req.on("data",contenido=>{
@@ -34,10 +33,48 @@ app.post("/api/cursos/programacion",(req,res)=>{
 
     });*/
 
-    console.log(req.body.titulo);
-    res.end("El servidor recibió una solicitud POST");
+    let cursoNuevo=req.body;
+    infoCursos.programacion.push(cursoNuevo);
+    res.send(JSON.stringify(infoCursos.programacion));
 
 });
+
+routerProgramacion.put("/:id",(req,res)=>{
+    const cursoActualizado=req.body;
+    const id=req.params.id;
+
+    const indice=infoCursos.programacion.findIndex(curso=>curso.id==id);
+
+    if(indice>=0){
+        infoCursos.programacion[id-1]=cursoActualizado
+    }
+    res.send(JSON.stringify(infoCursos.programacion));
+
+});
+
+routerProgramacion.patch("/:id", (req,res)=>{
+    const id=req.params.id;
+    const infoActualizada=req.body;
+
+    const indice=infoCursos.programacion.findIndex(curso=>curso.id==id);
+
+    if(indice>=0){
+        const cursoAModificar=infoCursos.programacion[indice];
+        Object.assign(cursoAModificar,infoActualizada);
+    }
+
+    res.send(JSON.stringify(infoCursos.programacion))
+});
+
+routerProgramacion.delete("/:id",(req,res)=>{
+    const id=req.params.id;
+    const indice=infoCursos.programacion.findIndex(curso => curso.id==id);
+
+    if(indice>=0){
+        infoCursos.programacion.splice(indice,1);
+    }
+    res.send(JSON.stringify(infoCursos.programacion));
+})
 
 
 const PUERTO=process.env.PORT || 3000;
